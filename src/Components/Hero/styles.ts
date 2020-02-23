aefc3045fd04662dad0ba6cc96afd58c335ebc3c
siddharthparmar7 <siddharthparmar7@gmail.com>
2020-02-23 14:55:39 -0800

    posts init setup

--- src/Components/Hero/styles.ts
+++ src/Components/Hero/styles.ts
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles(theme => ({
   root: {
     height: "550px",
-    // opacity: 0.6,
     display: "flex",
     flex: 1,
     alignItems: "center",
@@ -12,9 +11,7 @@ export const useStyles = makeStyles(theme => ({
     background:
       "linear-gradient(to right bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.3))"
   },
-  insideStyles: {
-    // maxWidth: "80%"
-  },
+  insideStyles: {},
   title: {
     color: "white",
     textTransform: "uppercase",
---
+++ src/Components/Posts/Posts.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Posts: React.FC<{}> = () => {
+  return <div />;
+};
+
+export default Posts;
